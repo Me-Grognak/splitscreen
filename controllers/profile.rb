bc72393c9f8db4e882a181b06733631fbb3907a2
Jeff Steed <steed.jeff@gmail.com>
2015-12-10 11:47:59 -0600

    Brought in existing profile values to profile edit

--- controllers/profile.rb
+++ controllers/profile.rb
@@ -1,29 +1,33 @@
 class ProfileController < ApplicationController
 
   get "/user/:user_name" do
-    @user_name = params[:user_name]
-    user = Account.find_by(user_name: @user_name)
-    @id = user.id
-    if session[:current_user] != nil
-      @poster_id = session[:current_user].id
-      @poster_name = session[:current_user].user_name
+    if !does_user_exist(params[:user_name])
+      erb :page_not_found
+    else
+      @user_name = params[:user_name]
+      user = Account.find_by(user_name: @user_name)
+      @id = user.id
+      if session[:current_user] != nil
+        @poster_id = session[:current_user].id
+        @poster_name = session[:current_user].user_name
+      end
+      comment = Comment.where(account_id: @id)
+      @comments = comment.reverse
+      if !authorization_check || session[:current_user].user_name != params[:user_name]
+        @no_edit = "none"
+      end
+      if session[:current_user] == nil
+        @no_post = "none"
+      end
+      erb :profile
     end
-    comment = Comment.where(account_id: @id)
-    @comments = comment.reverse
-    if !authorization_check || session[:current_user].user_name != params[:user_name]
-      @no_edit = "none"
-    end
-    if session[:current_user] == nil
-      @no_post = "none"
-    end
-    erb :profile
   end
 
 #----------------------- For Posting Comments ---------------------------------#
 
   post "/user/:user_name" do
 
-    new_comment = Comment.new(account_id: params[:account_id], poster_id: params[:poster_id], comment: params[:comment])
+    new_comment = Comment.new(account_id: params[:account_id], poster_name: params[:poster_name], comment: params[:comment])
     new_comment.save
 
     redirect "/profile/user/" + params[:user_name]
@@ -47,6 +51,7 @@ class ProfileController < ApplicationController
     if authorization_check && session[:current_user].user_name == params[:user_name]
       p @id = session[:current_user].id
       p @user_name = session[:current_user].user_name
+      @profile = Profile.find_by(account_id: @id)
       erb :edit_profile
     else
       erb :not_authorized
@@ -56,8 +61,8 @@ class ProfileController < ApplicationController
 #------------------------ Profile Editing Submissions -------------------------#
   post "/edit_profile/:user_name" do
 
-    profile = Profile.new(account_id: params[:account_id], location: params[:location], pc: params[:pc], ps4: params[:ps4], xbo: params[:xbo], wiiu: params[:wiiu], ps3: params[:ps3], xb360: params[:xb360], wii: params[:wii], steam_id: params[:steam_tag], psn_id: params[:psn_tag], xbl_id: params[:xbl_tag]);
-    profile.save
+    # profile = Profile.new(account_id: params[:account_id], location: params[:location], pc: params[:pc], ps4: params[:ps4], xbo: params[:xbo], wiiu: params[:wiiu], ps3: params[:ps3], xb360: params[:xb360], wii: params[:wii], steam_id: params[:steam_tag], psn_id: params[:psn_tag], xbl_id: params[:xbl_tag]);
+    # profile.save
 
     p profile
     redirect "/:user_name"
