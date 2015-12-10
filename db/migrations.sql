817fdd00ba9c7b74567c6ce7deeba91ee2248bff
Jason Bratt <jasonhero858@gmail.com>
2015-12-10 09:49:41 -0600

    fixing views/likes

--- db/migrations.sql
+++ db/migrations.sql
@@ -3,7 +3,7 @@ CREATE DATABASE splitscreen;
 \c splitscreen
 
 CREATE TABLE accounts (id SERIAL PRIMARY KEY, user_name VARCHAR(255), user_email VARCHAR(255), password_digest VARCHAR(255));
-CREATE TABLE account_images (id SERIAL PRIMARY KEY, user_name VARCHAR(255), image_path TEXT, title varchar(50), views INT, likes INT);
+CREATE TABLE account_images (id SERIAL PRIMARY KEY, user_name VARCHAR(255), image_path TEXT, title varchar(50), views INT DEFAULT 0, likes INT DEFAULT 0);
 CREATE TABLE image_likes (id SERIAL PRIMARY KEY, image_id INT, user_name VARCHAR(255));
 CREATE TABLE image_comments (id SERIAL PRIMARY KEY, image_id INT, account_id INT, user_name VARCHAR(255), comment TEXT, pos_y INT, pos_x INT, border varchar(40), color varchar(40));
 CREATE TABLE comments (id SERIAL PRIMARY KEY, account_id INT, comment TEXT, date_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP);
