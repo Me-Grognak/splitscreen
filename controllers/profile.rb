611a31ae8aa20e03eab5052129a555721791253f
Jeff Steed <steed.jeff@gmail.com>
2015-12-10 13:05:02 -0600

    Update profile working

--- controllers/profile.rb
+++ controllers/profile.rb
@@ -64,8 +64,12 @@ class ProfileController < ApplicationController
     # profile = Profile.new(account_id: params[:account_id], location: params[:location], pc: params[:pc], ps4: params[:ps4], xbo: params[:xbo], wiiu: params[:wiiu], ps3: params[:ps3], xb360: params[:xb360], wii: params[:wii], steam_id: params[:steam_tag], psn_id: params[:psn_tag], xbl_id: params[:xbl_tag]);
     # profile.save
 
+    profile = Profile.find_by(account_id: params[:account_id])
+    profile.update(location: params[:location], pc: params[:pc], ps4: params[:ps4], xbo: params[:xbo], wiiu: params[:wiiu], ps3: params[:ps3], xb360: params[:xb360], wii: params[:wii], steam_id: params[:steam_tag], psn_id: params[:psn_tag], xbl_id: params[:xbl_tag])
+    # profile.save
+
     p profile
-    redirect "/:user_name"
+    redirect "/profile/user/" + params[:user_name]
   end
 #------------------------------------------------------------------------------#
 
