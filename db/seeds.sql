6b74a14246068745160931a4bed38962dc11affe
Jeff Steed <steed.jeff@gmail.com>
2015-12-11 12:08:58 -0600

    Seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -127,3 +127,15 @@ INSERT INTO account_images (user_name, image_path, title, views, likes) VALUES (
 INSERT INTO account_images (user_name, image_path, title, views, likes) VALUES ('playerone', '083041ac6381d2a0eba865893f18010f.jpg', 'Testy test', 10, 12);
 INSERT INTO account_images (user_name, image_path, title, views, likes) VALUES ('playertwo', '083041ac6381d2a0eba865893f18010f.jpg', 'Testy test', 10, 12);
 INSERT INTO account_images (user_name, image_path, title, views, likes) VALUES ('playertwo', 'b2f7a827c03dcf980113a7a648c0b5a0.jpg', 'rawr rawr', 3, 18);
+
+INSERT INTO accounts (user_name, user_email, password_digest) VALUES ('jeffsteed', 'jeffsteed@email.com', 'password');
+INSERT INTO accounts (user_name, user_email, password_digest) VALUES ('jasonbratt', 'jasonbratt@email.com', 'password');
+INSERT INTO accounts (user_name, user_email, password_digest) VALUES ('jamestraver', 'jamestraver@email.com', 'password');
+INSERT INTO accounts (user_name, user_email, password_digest) VALUES ('rubyqueen', 'rubyqueen@email.com', 'password');
+INSERT INTO accounts (user_name, user_email, password_digest) VALUES ('jimbojones', 'jimbojones@email.com', 'password');
+
+INSERT INTO profiles (account_id, user_name, location, pc, ps4, xbo, wiiu, ps3, xb360, wii, steam_id, psn_id, xbl_id) VALUES (1, 'jeffsteed', 'Chicago, IL', true, true, false, false, true, true, false, 76561198011139872, 'OMALLYlive', 'OMALLYlive');
+INSERT INTO profiles (account_id, user_name, location, pc, ps4, xbo, wiiu, ps3, xb360, wii, steam_id, psn_id, xbl_id) VALUES (2, 'jasonbratt', 'Grand Rapids, MI', true, true, false, false, false, true, false, 76561198066391625, 'Jason_BraTT', 'INsane_Hero');
+INSERT INTO profiles (account_id, user_name, location, pc, ps4, xbo, wiiu, ps3, xb360, wii, steam_id, psn_id, xbl_id) VALUES (3, 'jamestraver', 'Atlanta, GA', true, false, false, false, true, true, false, 0, 'CodeForCoffee', 'Code_for_Coffee');
+INSERT INTO profiles (account_id, user_name, location, pc, ps4, xbo, wiiu, ps3, xb360, wii, steam_id, psn_id, xbl_id) VALUES (4, 'rubyqueen', 'San Francisco, CA', true, false, true, false, true, false, false, 0, 'RoobyQueen', 'Ruby_Queen');
+INSERT INTO profiles (account_id, user_name, location, pc, ps4, xbo, wiiu, ps3, xb360, wii, steam_id, psn_id, xbl_id) VALUES (5, 'jimbojones', 'Indianapolis, IN', true, true, false, true, false, true, false, 0, 'jim_1612', 'JIMboJOnes');
